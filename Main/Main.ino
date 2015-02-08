74f6e0422ec075b778fdd4ff8258ea4906eb65be
Stephen Gilardi <sgilardi8@hotmail.com>
2015-02-08 15:33:04 -0800

    Added Main
    Setup the file we will use as the primary driver. ONLY THIS FILE WILL
    BE UPLOADED TO MICROMOUSE.

---
+++ Main/Main.ino
@@ -0,0 +1,13 @@
+/*
+*  MAIN DRIVER for MicroMouse
+*  This is the only piece of code that will be uploaded to the Micromouse
+*  Team 3 - Winter 2015
+**/
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
