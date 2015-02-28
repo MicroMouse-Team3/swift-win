a4d42ba396566e81c15d13ce32707962dccf472a
vinceocam <viocampo@ucsd.edu>
2015-02-28 15:17:39 -0800

    Revert "Revert "IR Readings""
    This reverts commit 4b8f378c06f2d82ada0b93c03206188a4d80d460.

--- Main/Main.ino
+++ Main/Main.ino
@@ -4,12 +4,28 @@
 *  Team 3 - Winter 2015
 **/
 
+const int irLtOut = 0, irLtIn =0;
+const int irLtDiagOut =0,irLtDiagIn =0;
+const int irLtFrntOut = 0,irLtFrntIn = 0;  // need pins
+const int irRtOut =0, irRtIn = 0;
+const int irRtDiagOut = 0, irRtDiagIn = 0;
+const int irRtFrntOut = 0, irRtFrntIn = 0;
+
+unsigned long curt = 0; 
+
 void setup(){
   
+  pinMode(irLtOut,OUTPUT);       pinMode(irLtIn, INPUT);
+  pinMode(irLtDiagOut,OUTPUT);   pinMode(irLtDiagIn, INPUT);
+  pinMode(irLtFrntOut,OUTPUT);   pinMode(irLtFrntIn, INPUT);
+  pinMode(irRtOut,OUTPUT);       pinMode(irRtIn, INPUT);
+  pinMode(irRtDiagOut,OUTPUT);   pinMode(irRtDiagIn, INPUT);
+  pinMode(irRtFrntOut,OUTPUT);   pinMode(irRtFrntIn, INPUT);
+  Serial.begin(9600);
 }
 
 void loop(){
-  getIR();
+  readSensors();
   drive();
   PIDMain();
 }
@@ -63,32 +79,51 @@ void resetDistanceTraveled(){
 *
 **/
 
-void getIR(){
-  
+int getIR(int pinOut, int pinIn){
+  int senseVal = 0;
+  curt = micros();
+  digitalWrite(pinOut,HIGH);
+  while(micros()-curt <100);
+  senseVal = analogRead(pinIn);
+  digitalWrite(pinOut,LOW);
+  delay(1000); // just for testing
+  return senseVal;
+}
+
+void readSensors(){
+  getLeftIR();
+  delay(10); // must find out how long for other pulses to dissipate, improve accuracy
+  getLeftDiagIR();
+  getLeftFrontIR();
+  getRightIR();
+  getRightDiagIR();
+  getRightFrontIR();
 }
 
 int getLeftIR(){
+  return getIR(irLtOut,irLtIn);
   
 }
 
 int getLeftDiagIR(){
+  return getIR(irLtDiagOut,irLtDiagIn);
   
 }
 
 int getLeftFrontIR(){
-  
+  return getIR(irLtFrntOut,irLtFrntIn);
 }
 
 int getRightIR(){
-  
+   return getIR(irLtOut,irLtIn);
 }
 
 int getRightDiagIR(){
-  
+   return getIR(irRtDiagOut,irRtDiagIn);
 }
 
 int getRightFrontIR(){
-  
+   return getIR(irRtFrntOut,irRtFrntIn);
 }
 
 /*
@@ -109,5 +144,10 @@ int getRightEncod(){
   
 }
 
+ /*
+   Need help fixing this
+C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino/main.cpp:40: undefined reference to `setup'
+C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino/main.cpp:43: undefined reference to `loop'
+ */
 
 
