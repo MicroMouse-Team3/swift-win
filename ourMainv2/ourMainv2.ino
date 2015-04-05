594d79817cf60c1c3120f9484a8cb51c15935552
MrSwirlyEyes <colinkeef@gmail.com>
2015-04-04 22:21:22 -0700

    A SHIT TON OF CHANGES
    CLASSES ENABLED for Sensors and LEDs,
    
    I updated libraries, if you need these, let me know!

--- ourMainv2/ourMainv2.ino
+++ ourMainv2/ourMainv2.ino
@@ -1,3 +1,10 @@
+
+
+#include <Motor.h>
+#include <Encoder.h>
+#include <Sensor.h>
+#include <LED.h>
+
 /*
 *  MAIN for MicroMouse
 *  This is the only piece of code that will be uploaded to the Micromouse
@@ -5,21 +12,55 @@
 **/
 
 #include <math.h>
+#define NUMSENSORS 6
 //Pin Assignments for IR Sensors
-const int irLtOut = 3, irLtIn = A12, irLtLED = 11;
-const int irLtDiagOut = 28,irLtDiagIn = A18, irLtDiagLED = 30;
-const int irLtFrntOut = 4,irLtFrntIn = A11, irLtFrntLED = 13;
-const int irRtOut = 2, irRtIn = A13, irRtLED = 12;
-const int irRtDiagOut = 25, irRtDiagIn = A15, irRtDiagLED = 27 ;
-const int irRtFrntOut = 5, irRtFrntIn = A10, irRtFrntLED = 14;
+const byte leftEmitIR = 3 , leftRecIR = A12 , leftLED = 11;
+const byte leftDiagEmitIR = 28 , leftDiagRecIR = A18 , leftDiagLED = 30;
+const byte leftFrontEmitIR = 4 , leftFrontRecIR = A11 , leftFrontLED = 13;
 
-//Pin Assignments for Encoders
-const int RchA = 7, RchB = 8;
-const int LchA = 9, LchB = 10;
+const byte rightEmitIR = 2 , rightRecIR = A13 , rightLED = 12;
+const byte rightDiagEmitIR = 25 , rightDiagRecIR = A15 , rightDiagLED = 27 ;
+const byte rightFrontEmitIR = 5 , rightFrontRecIR = A10 , rightFrontLED = 14;
 
-//Pin Assignments for Motors
-const int LpwmA = A6, LpwmB = A7;
-const int RpwmA = A8, RpwmB = A9;
+/********** LEFT MOTOR VARS **********/
+
+//Left Enable            // Connection correlation to MM schematic
+const byte L_Enable = A2;  // (H-Bridge pin 1)
+
+//Motors
+const byte L_Mtr_A = A6;  // (H-Bridge pin 2)  //1A
+const byte L_Mtr_B = A7;  // (H-Bridge pin 7)  //2A
+
+//Encoder vars
+const byte L_CH_A = 9;          // (H-Bridge SV3 pin 6)
+const byte L_CH_B = 10;         // (H-Bridge SV3 pin 5)
+
+/********** RIGHT MOTOR VARS **********/
+
+//Right Enable            // Connection correlation to MM schematic
+const int R_Enable = A3;  // (H-Bridge pin ?)
+
+//Motors
+const int R_Mtr_A = A8;  // (H-Bridge pin ?)  //3A
+const int R_Mtr_B = A9;  // (H-Bridge pin ?)  //4A
+
+//Encoder vars
+int R_CH_A = 7;          // (H-Bridge SV3 pin ?)
+int R_CH_B = 8;         // (H-Bridge SV3 pin ?)
+
+
+//SENSOR Threshold Values
+unsigned int minThresh = 650;
+unsigned int maxThresh = 700;
+  
+int leftTicks = 0;
+int rightTicks = 0;
+
+
+Motor * mtrL;
+Motor * mtrR;
+Sensor * sensor[NUMSENSORS];
+
 
 //Global Boolean Values
 const bool debugOn = "TRUE";
@@ -34,21 +75,44 @@ unsigned long curt = 0;
 //Encoder Information
 volatile int state = LOW;
 
+
 void setup(){
   Serial.begin(9600); //Used for Debugging
   if(debugOn){
     Serial.println("->setup");
   }
-  
+
+  //Trash taken care of in classes
+  /*
   pinMode(irLtOut,OUTPUT);       pinMode(irLtIn, INPUT), pinMode(irLtLED, OUTPUT);
   pinMode(irLtDiagOut,OUTPUT);   pinMode(irLtDiagIn, INPUT), pinMode(irLtDiagLED, OUTPUT);
   pinMode(irLtFrntOut,OUTPUT);   pinMode(irLtFrntIn, INPUT), pinMode(irLtFrntLED, OUTPUT);
   pinMode(irRtOut,OUTPUT);       pinMode(irRtIn, INPUT), pinMode(irRtLED, OUTPUT);
   pinMode(irRtDiagOut,OUTPUT);   pinMode(irRtDiagIn, INPUT), pinMode(irRtDiagLED, OUTPUT);
   pinMode(irRtFrntOut,OUTPUT);   pinMode(irRtFrntIn, INPUT), pinMode(irRtFrntLED, OUTPUT);
+  */
+  mtrL = new Motor( L_Enable , L_Mtr_A , L_Mtr_B , L_CH_A , L_CH_B );  
+  mtrR = new Motor( R_Enable , R_Mtr_A , R_Mtr_B , R_CH_A , R_CH_B );
+  
+  //Left left
+  sensor[0] = new Sensor( leftEmitIR , leftRecIR , leftLED );
+  //Left Diag
+  sensor[1] = new Sensor( leftDiagEmitIR , leftDiagRecIR , leftDiagLED );
+  //Left Forward
+  sensor[2] = new Sensor( leftFrontEmitIR , leftDiagRecIR , leftDiagLED );
+  //Right Forward
+  sensor[3] = new Sensor( rightEmitIR , rightRecIR , rightLED );
+  //Right Diag
+  sensor[4] = new Sensor( rightDiagEmitIR , rightDiagRecIR , rightDiagLED );
+  //Right right
+  sensor[5] = new Sensor( rightFrontEmitIR , rightFrontRecIR , rightFrontLED ); 
+  
   
   attachInterrupt(RchA, encoderTick, CHANGE);
   
+  
+  delay(7000);
+  setBothMtrsForward();
 }
 
 void loop(){
@@ -113,13 +177,14 @@ void turnRight(){
 }
 
 void goStraight(int leftPWM, int rightPWM){
+  setBothMtrsForward();
    if(debugOn){
      Serial.println("->goStraight"); //Used for Debugging 
   }
   mtrL->setSpeed(leftPWM);
   mtrR->setSpeed(rightPWM);
-  analogWrite(RpwmA, rightpwm);
-  analogWrite(LpwmA, leftpwm);
+//  analogWrite(RpwmA, rightpwm);
+//  analogWrite(LpwmA, leftpwm);
 }
 
 void turnAround(){
@@ -147,24 +212,12 @@ void resetDistanceTraveled(){
 *
 **/
 
-double getIR(int pinOut, int pinIn){
-  if(debugOn){
-    Serial.println("->getIR"); //Used for Debugging 
-  }
- 
-  double senseVal = 0;
-  curt = micros();
-  digitalWrite(pinOut,HIGH);
-  delayMicroseconds(80);
-  senseVal = sensorData();
-  digitalWrite(pinOut,LOW);
-  return getCentiDistance(senseVal);
-}
-
-void readSensors(){
+void readDasSensors(){
   if(debugOn){
     Serial.println("->readSensors"); //Used for Debugging 
   }
+  
+  
   getLeftIR();
   getLeftDiagIR();
   getLeftFrontIR();
@@ -178,7 +231,7 @@ void readSensors(){
 * - increasing numReading increases accuracy, but increasing by 5 increases calculation time by ~500us
 */
 
-double sensorData(){
+double sensorData(int readPin){
   int numReadings = 15;
   int readings[numReadings];
   int index = 0;
@@ -189,7 +242,7 @@ double sensorData(){
     readings[thisReading] = 0;   
   while(run){
     total = total -readings[index];
-    readings[index] = analogRead(A0);
+    readings[index] = analogRead(readPin);
     total = total + readings[index];
     index++;
     if(index >= numReadings){
@@ -221,11 +274,27 @@ double getCentiDistance(double irSensorValue){
     return 0.0;
 }
 
+double getIR(int emitPin, int recPin){
+  if(debugOn){
+    Serial.println("->getIR"); //Used for Debugging 
+  }
+ 
+  double senseVal = 0;
+  curt = micros();
+  sensor[0]->getLED().setHIGH();  
+//  digitalWrite(pinOut,HIGH); //LED
+  delayMicroseconds(80);
+  senseVal = sensorData(sensor[0]->getRecPin());
+  sensor[0]->getLED().setLOW();  
+//  digitalWrite(pinOut,LOW); //LED
+  return getCentiDistance(senseVal);
+}
+
 double getLeftIR(){
   if(debugOn){
     Serial.println("->getLeftIR"); //Used for Debugging 
   }
-  return getIR(irLtOut,irLtIn);
+  return getIR( leftEmitIR , leftRecIR );
   
 }
 
@@ -233,7 +302,7 @@ int getLeftDiagIR(){
   if(debugOn){
     Serial.println("->getLeftDiagIR"); //Used for Debugging 
   }
-  return getIR(irLtDiagOut,irLtDiagIn);
+  return getIR( leftDiagEmitIR , leftDiagRecIR );
   
 }
 
@@ -241,28 +310,28 @@ int getLeftFrontIR(){
   if(debugOn){
     Serial.println("->getLeftFrontIR"); //Used for Debugging 
   }
-  return getIR(irLtFrntOut,irLtFrntIn);
+  return getIR( leftFrontEmitIR , leftFrontRecIR );
 }
 
 int getRightIR(){
   if(debugOn){
     Serial.println("->getRightIR"); //Used for Debugging 
   }
-   return getIR(irLtOut,irLtIn);
+   return getIR( leftEmitIR , leftRecIR );
 }
 
 int getRightDiagIR(){
   if(debugOn){
     Serial.println("->getRightDiagIR"); //Used for Debugging 
   }
-   return getIR(irRtDiagOut,irRtDiagIn);
+   return getIR( rightDiagEmitIR , rightDiagRecIR );
 }
 
 int getRightFrontIR(){
   if(debugOn){
     Serial.println("->getRightFrontIR"); //Used for Debugging 
   }
-   return getIR(irRtFrntOut,irRtFrntIn);
+   return getIR( rightFrontEmitIR , rightFrontRecIR );
 }
 
 /*
@@ -271,6 +340,9 @@ int getRightFrontIR(){
 *
 **/
 
+
+//These are trash
+/*
 boolean tick(){
   if(debugOn){
     Serial.println("->tick"); //Used for Debugging 
@@ -289,4 +361,78 @@ int getRightEncod(){
   if(debugOn){
     Serial.println("->getRightEncod"); //Used for Debugging 
   }
+}
+*/
+
+// MrSwirlyEye's functions (to rid of initialize.h
+
+void readSensors() {
+  for ( byte i = 0 ; i < NUMSENSORS ; i++ )
+    sensor[i]->setSensorRead(  analogRead( sensor[i]->getRecPin() )  );
+}
+
+void printSensorReadings() {
+  for ( byte i = 0 ; i < NUMSENSORS ; i++ ) {
+    Serial.print("Sensor#");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.print(sensor[i]->getSensorRead());    
+    
+    if ( i == (NUMSENSORS - 1) )
+      Serial.println();
+    else
+      Serial.print(" // ");
+  }     
+}
+
+void sensorsToLEDs() {
+  for ( byte i = 0 ; i < NUMSENSORS ; i++ ) {
+    if ( sensor[i]->getSensorRead() < minThresh )
+      sensor[i]->getLED().setLOW();
+    else
+      sensor[i]->getLED().setHIGH();
+  }      
+}
+
+/********** MOTOR FUNCTIONS **********/
+
+void setLMtrSpd( byte speed ) {
+  mtrL->setSpeed(speed);     
+}
+
+void setRMtrSpd( byte speed ) {
+  mtrR->setSpeed(speed);
+}
+
+void setBothMtrsSpd( byte speed ) {
+  mtrL->setSpeed(speed);
+  mtrR->setSpeed(speed);
+}
+
+void setBothMtrsForward() {
+  mtrL->setForward();
+  mtrR->setForward();
+}
+
+void setBothMtrsBackward() {
+  mtrL->setBackward();
+  mtrR->setBackward();
+}
+
+void setMtrsLeftTurn() {
+  mtrL->setBackward();
+  mtrR->setForward();
+}
+
+void setMtrsRightTurn() {
+  mtrL->setForward();
+  mtrR->setBackward();
+} 
+
+
+/********** ENCODER FUNCTIONS **********/
+
+void readBothEnc() {
+  mtrL->getEnc().readEnc();
+  mtrR->getEnc().readEnc();
 }
